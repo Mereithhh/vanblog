69c8e30655c2ab35d2e6e721474e04fa558b3e0d
wanglu <wanglu@mereith.com>
2022-06-15 09:58:45 +0000

    feat: 增加初始化管道&未初始化自动重定向至 /admin/init

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { InitMiddleware } from './provider/auth/init.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
+  app.use(InitMiddleware);
   await app.listen(3000);
   console.log('应用已启动，端口: 3000');
   console.log('API 端点地址: http://localhost:3000/api');
---
+++ packages/server/src/provider/auth/init.middleware.ts
@@ -0,0 +1,17 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { InitProvider } from '../init/init.provider';
+
+@Injectable()
+export class InitMiddleware implements NestMiddleware {
+  constructor(private readonly initProvider: InitProvider) {}
+  use(req: Request, res: Response, next: NextFunction) {
+    this.initProvider.checkHasInited().then((hasInit) => {
+      if (hasInit) {
+        next();
+      } else {
+        res.redirect('/admin/init');
+      }
+    });
+  }
+}
