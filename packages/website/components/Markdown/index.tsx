c6f70f65582ea1951e184ee330f1fe4ae2ae5d82
wanglu <wanglu@mereith.com>
2022-07-16 18:21:59 +0800

    feat: 完善代码高亮"

--- packages/website/components/Markdown/index.tsx
+++ packages/website/components/Markdown/index.tsx
@@ -1,7 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 export default function (props: { content: string }) {
   return (
@@ -10,12 +10,13 @@ export default function (props: { content: string }) {
         components={{
           code({ node, inline, className, children, ...props }) {
             const match = /language-(\w+)/.exec(className || "");
+            console.log(children);
             return !inline && match ? (
               <SyntaxHighlighter
                 children={String(children).replace(/\n$/, "")}
-                style={oneLight as any}
+                style={vscDarkPlus as any}
                 language={match[1]}
-                PreTag="div"
+                PreTag="pre"
                 {...props}
               />
             ) : (
