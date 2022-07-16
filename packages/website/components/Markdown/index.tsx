3a05fae7febc6c2c07e7d25d4700f2f9bc5ab113
wanglu <wanglu@mereith.com>
2022-07-16 18:04:43 +0800

    feat； 代码高亮

--- packages/website/components/Markdown/index.tsx
+++ packages/website/components/Markdown/index.tsx
@@ -1,10 +1,30 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import rehypeHighlight from "rehype-highlight";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
 export default function (props: { content: string }) {
   return (
     <>
       <ReactMarkdown
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, "")}
+                style={oneLight as any}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
         className="markdown-body"
         children={props.content}
         remarkPlugins={[remarkGfm]}
