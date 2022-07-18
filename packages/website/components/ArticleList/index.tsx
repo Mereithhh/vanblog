94ae4f093ae2a25bb9d459d0131c266aa456a032
wanglu <wanglu@mereith.com>
2022-07-18 23:16:06 +0800

    feat: 时间线溢出格式

--- packages/website/components/ArticleList/index.tsx
+++ packages/website/components/ArticleList/index.tsx
@@ -8,15 +8,15 @@ export default function (props: { articles: Article[]; showYear?: boolean }) {
         return (
           <Link href={`/post/${article.id}`} key={article.id}>
             <a
-              className="dark:border-dark-2 dark:hover:border-nav-dark-light flex items-center border-b pb-1 border-dashed cursor-pointer group border-gray-200 hover:border-gray-400 "
+              className="dark:border-dark-2  dark:hover:border-nav-dark-light flex items-center border-b pb-1 border-dashed cursor-pointer group border-gray-200 hover:border-gray-400 "
               key={article.id}
             >
-              <div className="text-gray-400 text-base group-hover:text-gray-600 dark:text-dark-400 dark:group-hover:text-dark-light">
+              <div className="text-gray-400 flex-grow-0 flex-shrink-0 text-sm  group-hover:text-gray-600 dark:text-dark-400 dark:group-hover:text-dark-light">
                 {dayjs(article.createdAt).format(
                   props.showYear ? "YYYY-MM-DD" : "MM-DD"
                 )}
               </div>
-              <div className="ml-4 text-lg text-gray-600 group-hover:text-gray-800 dark:text-dark dark:group-hover:text-dark">
+              <div className="ml-2 md:ml-4 text-base flex-grow flex-shrink overflow-hidden text-gray-600 group-hover:text-gray-800 dark:text-dark dark:group-hover:text-dark">
                 {article.title}
               </div>
             </a>
