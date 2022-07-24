1c0b481f4fde63d46b9bb960dd0d456999092919
wanglu <wanglu@mereith.com>
2022-07-24 11:52:16 +0800

    doc: 增加搜索插件'

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,12 +1,20 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme";
-
+import { searchPlugin } from "@vuepress/plugin-search";
 export default defineUserConfig({
   lang: "zh-CN",
   title: "Van Blog",
   description: "Van Blog 的官方网站",
   head: [["link", { rel: "icon", href: "/logo.svg" }]],
   base: "/",
-
+  plugins: [
+    searchPlugin({
+      locales: {
+        "/": {
+          placeholder: "搜索",
+        },
+      },
+    }),
+  ],
   theme,
 });
--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -86,10 +86,10 @@ export default hopeTheme({
        * Using Giscus
        */
       provider: "Giscus",
-      repo: "vuepress-theme-hope/giscus-discussions",
-      repoId: "R_kgDOG_Pt2A",
+      repo: "mereithhh/vanblog-comment",
+      repoId: "R_kgDOHtQfpQ",
       category: "Announcements",
-      categoryId: "DIC_kwDOG_Pt2M4COD69",
+      categoryId: "DIC_kwDOHtQfpc4CQZcs",
 
       /**
        * Using Twikoo
