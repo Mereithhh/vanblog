c42e9989e9df230ce345936cf1f8c14a28e74a51
WangLu <cs3cx4g@gmail.com>
2022-07-24 02:54:55 +0000

    ci: 更新构建配置

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-nohup node /app/server/main.js  > /var/log/vanblog-server.log 2>&1 &
+nginx
 nohup node /app/website/server.js -p 3001 > /var/log/vanblog-website.log 2>&1 &
+# nohup node /app/server/main.js  > /var/log/vanblog-server.log 2>&1 &
+node /app/server/main.js 
 
-nginx -g "daemon off;"
+
