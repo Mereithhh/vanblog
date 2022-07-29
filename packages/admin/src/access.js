901f034f925a20fec56d0ca0bd68655b42ce5c02
wanglu <wanglu@mereith.com>
2022-07-29 14:23:08 +0800

    format: 去掉后台冗余鉴权插件

--- packages/admin/src/access.js
+++
@@ -1,9 +0,0 @@
-/**
- * @see https://umijs.org/zh-CN/plugins/plugin-access
- * */
-export default function access(initialState) {
-  const { currentUser } = initialState ?? {};
-  return {
-    canAdmin: currentUser && currentUser.access === 'admin',
-  };
-}
