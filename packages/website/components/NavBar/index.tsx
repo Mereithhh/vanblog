0f414bc639c1c9f0744163f13976398579357777
wanglu <wanglu@mereith.com>
2022-07-30 21:51:55 +0800

    format: 移除无用代码

--- packages/website/components/NavBar/index.tsx
+++ packages/website/components/NavBar/index.tsx
@@ -91,7 +91,7 @@ export default function (props: {
                   p-id="1340"
                   width="24"
                   height="24"
-                  className="dark:text-dark"
+                  className="dark:text-dark fill-gray-600"
                 >
                   <path
                     d="M904 160H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zM904 784H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8zM904 472H120c-4.4 0-8 3.6-8 8v64c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-64c0-4.4-3.6-8-8-8z"
