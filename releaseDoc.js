da05ee9555a86b5bc6e2e6e5b02d61c37b344ba1
wanglu <wanglu@mereith.com>
2022-08-22 09:51:21 +0000

    docs: 更新文档

---
+++ releaseDoc.js
@@ -0,0 +1,29 @@
+const insertLog = () => {
+  const fs = require("fs");
+  const log = fs.readFileSync("CHANGELOG.md", { encoding: "utf-8" });
+  const newLog = "---\ntitle: 更新日志\nicon: update\n---\n\n" + log;
+
+  fs.writeFileSync("docs/ref/changelog.md", newLog, { encoding: "utf-8" });
+
+  let version = fs.readFileSync("doc-version", { encoding: "utf-8" });
+  version = version.split("\n")[0].trim();
+  const arr = version.split(".");
+  const sub = arr.pop();
+  arr.push(String(parseInt(sub) + 1));
+  const newVersion = arr.join(".");
+  fs.writeFileSync("doc-version", newVersion, { encoding: "utf-8" });
+
+  // 添加并应用
+  const { execSync } = require("child_process");
+  execSync(
+    `git add . && git commit -m 'docs: 更新文档' && git tag doc-${newVersion} && git push --tags`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    }
+  );
+};
+insertLog();
