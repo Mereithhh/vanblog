a1826f8270e263689a6015f6162ec976f2622c1d
wanglu <wanglu@mereith.com>
2022-08-23 04:36:11 +0000

    fix: 还是不展示大纲

--- packages/admin/src/components/Editor/index.tsx
+++ packages/admin/src/components/Editor/index.tsx
@@ -248,10 +248,6 @@ export default function Editor(props) {
           enable: true,
         },
         height: '100%',
-        outline: {
-          enable: true,
-          position: 'left',
-        },
       });
     }
 
