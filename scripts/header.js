891f1b069a7ee8904217db4c0b6cbb538f29ef18
wanglu <wanglu@mereith.com>
2022-08-26 10:14:41 +0000

    ci: 优化 caddyfile 反代配置

---
+++ scripts/header.js
@@ -0,0 +1,30 @@
+const http = require("http");
+
+http
+  .createServer((request, response) => {
+    const { headers, method, url } = request;
+    let body = [];
+    request
+      .on("error", (err) => {
+        console.error(err);
+      })
+      .on("data", (chunk) => {
+        body.push(chunk);
+      })
+      .on("end", () => {
+        body = Buffer.concat(body).toString();
+        // BEGINNING OF NEW STUFF
+        console.log(JSON.stringify(headers, null, 2));
+        response.on("error", (err) => {
+          console.error(err);
+        });
+
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "application/json");
+        const responseBody = { headers, method, url, body };
+
+        response.write(JSON.stringify(responseBody));
+        response.end();
+      });
+  })
+  .listen(1337);
