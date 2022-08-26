31fca3be64e84baae855a713f673c0bd7c5f90bd
wanglu <wanglu@mereith.com>
2022-08-26 06:29:54 +0000

    fix: 初始化不触发增量渲染

--- packages/server/src/controller/admin/init/init.controller.ts
+++ packages/server/src/controller/admin/init/init.controller.ts
@@ -10,6 +10,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags } from '@nestjs/swagger';
 import { InitDto } from 'src/dto/init.dto';
 import { InitProvider } from 'src/provider/init/init.provider';
+import { ISRProvider } from 'src/provider/isr/isr.provider';
 import { StaticProvider } from 'src/provider/static/static.provider';
 
 @ApiTags('init')
@@ -18,6 +19,7 @@ export class InitController {
   constructor(
     private readonly initProvider: InitProvider,
     private readonly staticProvider: StaticProvider,
+    private readonly isrProvider: ISRProvider,
   ) {}
 
   @Post('/init')
@@ -27,6 +29,7 @@ export class InitController {
       throw new HttpException('已初始化', 500);
     }
     await this.initProvider.init(initDto);
+    this.isrProvider.activeAll('初始化触发增量渲染！');
     return {
       statusCode: 200,
       message: '初始化成功!',
