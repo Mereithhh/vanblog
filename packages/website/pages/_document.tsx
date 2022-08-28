89dfdd600dacea976e64bf0d43ce43c3c85d5d71
wanglu <wanglu@mereith.com>
2022-08-28 12:06:26 +0000

    fix: 设置站点图标失效

--- packages/website/pages/_document.tsx
+++ packages/website/pages/_document.tsx
@@ -1,11 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html className="light" lang="zh">
-      <Head>
-        <link rel="icon" href="/logo.svg"></link>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
