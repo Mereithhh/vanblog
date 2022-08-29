570a527de33e839363bf216caf2445df18708f45
wanglu <wanglu@mereith.com>
2022-08-29 08:47:52 +0000

    chore: 修正 releaseNote 脚本错误

--- scripts/releaseNote.js
+++ scripts/releaseNote.js
@@ -2,10 +2,12 @@ const fs = require("fs");
 const run = () => {
   const changelog = fs.readFileSync("CHANGELOG.md", { encoding: "utf-8" });
   const c = changelog
-    .split("### [")
+    .split("## [")
     .slice(0, 2)
-    .join("### [")
-    .replace("# Changelog", "");
+    .join("## [")
+    .replace("# Changelog", "")
+    .substring(0, c.length - 1);
+
   fs.writeFileSync("CHANGE.md", c, { encoding: "utf-8" });
 };
 run();
