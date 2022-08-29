78bac4735e59067f84c6c83137d6db23592e10d7
wanglu <wanglu@mereith.com>
2022-08-29 04:35:28 +0000

    chore: 发版自动生成 releaseNote

---
+++ scripts/releaseNote.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const run = () => {
+  const changelog = fs.readFileSync("CHANGELOG.md", { encoding: "utf-8" });
+  const c = changelog
+    .split("### [")
+    .slice(0, 2)
+    .join("### [")
+    .replace("# Changelog", "");
+  fs.writeFileSync("CHANGE.md", c, { encoding: "utf-8" });
+};
+run();
