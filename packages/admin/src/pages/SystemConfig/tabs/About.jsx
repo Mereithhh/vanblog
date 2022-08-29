2e5b47208d5981ad7c2e770fe73c6eff762ce7a7
wanglu <wanglu@mereith.com>
2022-08-29 06:50:26 +0000

    feat: 后台关于页面增加 loading

--- packages/admin/src/pages/SystemConfig/tabs/About.jsx
+++ packages/admin/src/pages/SystemConfig/tabs/About.jsx
@@ -1,5 +1,5 @@
 import ProCard from '@ant-design/pro-card';
-import { Image, Space, Tag } from 'antd';
+import { Image, Space, Spin, Tag } from 'antd';
 import { useMemo } from 'react';
 import { useModel } from 'umi';
 export default function (props) {
@@ -8,33 +8,35 @@ export default function (props) {
     let v = initialState?.version || '获取中';
     return v;
   }, [initialState, history]);
+
   return (
-    <ProCard>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column',
-          userSelect: 'none',
-        }}
-      >
-        <Image width={200} src="/logo.svg" alt="logo" preview={false} />
+    <Spin spinning={version == '获取中'}>
+      <ProCard>
         <div
           style={{
-            fontSize: 26,
-            fontWeight: 500,
-            marginBottom: 8,
             display: 'flex',
             alignItems: 'center',
+            flexDirection: 'column',
+            userSelect: 'none',
           }}
         >
-          <div>VanBlog</div>
-          <div style={{ marginBottom: 4, marginLeft: 4 }}>
-            <Tag color="cyan">{version}</Tag>
+          <Image width={200} src="/logo.svg" alt="logo" preview={false} />
+          <div
+            style={{
+              fontSize: 26,
+              fontWeight: 500,
+              marginBottom: 8,
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <div>VanBlog</div>
+            <div style={{ marginBottom: 4, marginLeft: 4 }}>
+              <Tag color="cyan">{version}</Tag>
+            </div>
           </div>
-        </div>
-        <p align="center">一款简洁实用优雅的高性能个人博客系统</p>
-        <p>
+          <p align="center">一款简洁实用优雅的高性能个人博客系统</p>
+
           <Space>
             <a target={'_blank'} rel="noreferrer" href="https://github.com/Mereithhh/van-blog">
               Github
@@ -53,8 +55,8 @@ export default function (props) {
               提交BUG
             </a>
           </Space>
-        </p>
-      </div>
-    </ProCard>
+        </div>
+      </ProCard>
+    </Spin>
   );
 }
