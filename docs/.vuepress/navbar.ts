2e342b6940d3440a6cb615ed4737e997fcff1b05
wanglu <wanglu@mereith.com>
2022-08-31 21:49:40 +0800

    docs: 修复项目主页导航栏坏链

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,7 +3,7 @@ import { navbar } from "vuepress-theme-hope";
 export default navbar([
   { text: "介绍", link: "/intro", icon: "advance" },
   { text: "快速上手", icon: "creative", link: "/guide/docker" },
-  { text: "功能", link: "/feature/article", icon: "launch" },
+  { text: "功能", link: "/feature/basic/article", icon: "launch" },
   { text: "参考", link: "/ref/env", icon: "list" },
   { text: "Demo", link: "https://blog-demo.mereith.com", icon: "navigate" },
 ]);
