10db49c7938a148c2b5d91bebc35fdf1a2046a99
wanglu <wanglu@mereith.com>
2022-09-28 11:56:49 +0800

    fix: 特殊情形 markdown 标题解析失败会阻塞前台页面更新

--- packages/website/components/Markdown/tools.ts
+++ packages/website/components/Markdown/tools.ts
@@ -1,12 +1,25 @@
 export const getChildrenText = (children: any): any => {
+  if (!children) {
+    return "";
+  }
   let result = "";
-  children.forEach((c: any) => {
-    if (typeof c == "string") {
-      result = result + c;
-    }
-    if (typeof c == "object") {
-      result = result + c?.props?.children;
-    }
-  });
-  return result;
+  try {
+    children.forEach((c: any) => {
+      if (typeof c == "string") {
+        result = result + c;
+      } else if (typeof c == "object") {
+        if (c?.props?.children && typeof c?.props?.children == "string") {
+          result = result + c?.props?.children;
+        }
+      } else {
+        if (c?.props?.children && typeof c?.props?.children == "string") {
+          result = result + c?.props?.children;
+        }
+      }
+    });
+    return result;
+  } catch (err) {
+    console.log("获取 heading text 失败：", JSON.stringify(err, null, 2));
+    return result || "";
+  }
 };
