ae8cae4693f666b8bc3e35f638e6d81b731adf2c
wanglu <wanglu@mereith.com>
2022-10-24 17:38:25 +0800

    feat: 优化代码块样式: 减少滚动条高度、增加语言显示、复制按钮置顶以免挡住代码 #72 #71 #73

--- packages/website/components/Markdown/Code.tsx
+++ packages/website/components/Markdown/Code.tsx
@@ -14,32 +14,41 @@ export function CodeBlock(props: { children: any; match: any }) {
   return (
     <>
       <div className="relative">
-        <CopyToClipboard
-          text={String(props.children)}
-          onCopy={() => {
-            toast.success("复制成功！", {
-              className: "toast",
-            });
-          }}
+        <div
+          className="absolute flex items-center"
+          style={{ top: "2px", right: "2px", color: "#6f7177" }}
         >
-          <span
-            className="transition-all rounded inline-block cursor-pointer absolute top-2 right-2  bg-inherit hover:bg-gray-200  dark:hover:bg-gray-700"
-            style={{ padding: "6px 10px" }}
-          >
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 15 19"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11.0475 0.905273H1.67197C0.812542 0.905273 0.109375 1.60844 0.109375 2.46787V13.406H1.67197V2.46787H11.0475V0.905273ZM13.3914 4.03046H4.79716C3.93773 4.03046 3.23456 4.73363 3.23456 5.59306V16.5312C3.23456 17.3906 3.93773 18.0938 4.79716 18.0938H13.3914C14.2509 18.0938 14.954 17.3906 14.954 16.5312V5.59306C14.954 4.73363 14.2509 4.03046 13.3914 4.03046ZM13.3914 16.5312H4.79716V5.59306H13.3914V16.5312Z"
-                fill="#6f7177"
-              ></path>
-            </svg>
+          <span style={{ marginRight: 4 }}>
+            {props.match?.length ? props.match[1] : undefined}
           </span>
-        </CopyToClipboard>
+          <CopyToClipboard
+            text={String(props.children)}
+            onCopy={() => {
+              toast.success("复制成功！", {
+                className: "toast",
+              });
+            }}
+          >
+            <span
+              className="transition-all  rounded inline-block cursor-pointer  bg-inherit hover:bg-gray-200  dark:hover:bg-gray-700"
+              style={{ padding: "4px 8px" }}
+            >
+              <svg
+                width="14"
+                height="14"
+                viewBox="0 0 15 19"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.0475 0.905273H1.67197C0.812542 0.905273 0.109375 1.60844 0.109375 2.46787V13.406H1.67197V2.46787H11.0475V0.905273ZM13.3914 4.03046H4.79716C3.93773 4.03046 3.23456 4.73363 3.23456 5.59306V16.5312C3.23456 17.3906 3.93773 18.0938 4.79716 18.0938H13.3914C14.2509 18.0938 14.954 17.3906 14.954 16.5312V5.59306C14.954 4.73363 14.2509 4.03046 13.3914 4.03046ZM13.3914 16.5312H4.79716V5.59306H13.3914V16.5312Z"
+                  fill="#6f7177"
+                ></path>
+              </svg>
+            </span>
+          </CopyToClipboard>
+        </div>
+
         <SyntaxHighlighter
           style={theme.includes("dark") ? dark : (light as any)}
           language={props.match?.length ? props.match[1] : undefined}
