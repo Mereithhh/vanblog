f87fa247dc96afec4ad593e47f311b494032df8e
CornWorld <43746030+CornWorld@users.noreply.github.com>
2023-01-31 00:32:10 +0800

    [Feat] Add more environment variables for the database

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 echo "欢迎使用 VanBlog 博客系统"
+
+# Process Database Url
+if ! [ $VAN_BLOG_DATABASE_URL ] || [ -z $VAN_BLOG_DATABASE_URL ]; then
+  if [ $VAN_BLOG_DATABASE_USER ]; then
+    authInfo="${VAN_BLOG_DATABASE_USER}"
+    if [ $VAN_BLOG_DATABASE_PASSWD ]; then
+      authInfo="$authInfo:$VAN_BLOG_DATABASE_PASSWD"
+    fi
+    authInfo="$authInfo@"
+  fi
+  export VAN_BLOG_DATABASE_URL="mongodb://${authInfo-""}${VAN_BLOG_DATABASE_HOST-"mongo"}:${VAN_BLOG_DATABASE_PORT-"27017"}/${VAN_BLOG_DATABASE_DBNAME-"vanBlog"}?authSource=admin"
+fi
+
 echo "mongo_url: ${VAN_BLOG_DATABASE_URL}"
 sed "s/VAN_BLOG_EMAIL/${EMAIL}/g" /app/CaddyfileTemplate >/app/Caddyfile
 caddy start --config /app/Caddyfile
