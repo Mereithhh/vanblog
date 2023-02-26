031b1e8cb3b22b75e4f24965a184b6b249f9eddc
Mr.Hope <mister-hope@outlook.com>
2023-02-26 16:29:43 +0800

    docs: update docs

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,14 +1,14 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  { text: "介绍", link: "/intro", icon: "advance" },
+  { text: "介绍", icon: "advance", link: "/intro" },
   { text: "快速上手", icon: "creative", link: "/guide/docker" },
-  { text: "功能", link: "/feature/basic/article", icon: "launch" },
-  { text: "参考", link: "/ref/env", icon: "list" },
-  { text: "Demo", link: "https://blog-demo.mereith.com", icon: "navigate" },
+  { text: "功能", icon: "launch", link: "/feature/basic/article" },
+  { text: "参考", icon: "list", link: "/ref/env" },
+  { text: "Demo", icon: "navigate", link: "https://blog-demo.mereith.com" },
   {
     text: "交流群",
-    link: "https://jq.qq.com/?_wv=1027&k=5NRyK2Sw",
     icon: "support",
+    link: "https://jq.qq.com/?_wv=1027&k=5NRyK2Sw",
   },
 ]);
