8bae0677981ceee6c620071f1ceb6e9be983cef2
Mr.Hope <mister-hope@outlook.com>
2023-02-26 16:29:43 +0800

    docs: update docs

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -32,83 +32,21 @@ export default hopeTheme({
 
   pageInfo: ["Author", "Original", "Date", "Category", "Tag", "ReadingTime"],
 
-  // blog: {
-  //   description: "一个前端开发者",
-  //   intro: "/intro.html",
-  //   medias: {
-  //     Baidu: "https://example.com",
-  //     Bitbucket: "https://example.com",
-  //     Dingding: "https://example.com",
-  //     Discord: "https://example.com",
-  //     Dribbble: "https://example.com",
-  //     Email: "https://example.com",
-  //     Evernote: "https://example.com",
-  //     Facebook: "https://example.com",
-  //     Flipboard: "https://example.com",
-  //     Gitee: "https://example.com",
-  //     GitHub: "https://example.com",
-  //     Gitlab: "https://example.com",
-  //     Gmail: "https://example.com",
-  //     Instagram: "https://example.com",
-  //     Lines: "https://example.com",
-  //     Linkedin: "https://example.com",
-  //     Pinterest: "https://example.com",
-  //     Pocket: "https://example.com",
-  //     QQ: "https://example.com",
-  //     Qzone: "https://example.com",
-  //     Reddit: "https://example.com",
-  //     Rss: "https://example.com",
-  //     Steam: "https://example.com",
-  //     Twitter: "https://example.com",
-  //     Wechat: "https://example.com",
-  //     Weibo: "https://example.com",
-  //     Whatsapp: "https://example.com",
-  //     Youtube: "https://example.com",
-  //     Zhihu: "https://example.com",
-  //   },
-  // },
-
-  // encrypt: {
-  //   config: {
-  //     "/guide/encrypt.html": ["1234"],
-  //   },
-  // },
-
   plugins: {
     blog: false,
     copyCode: {},
 
-    // 如果你不需要评论，可以直接删除 comment 配置，
-    // 以下配置仅供体验，如果你需要评论，请自行配置并使用自己的环境，详见文档。
-    // 为了避免打扰主题开发者以及消耗他的资源，请不要在你的正式环境中直接使用下列配置!!!!!
     comment: {
-      /**
-       * Using Giscus
-       */
       provider: "Giscus",
       repo: "mereithhh/vanblog-comment",
       repoId: "R_kgDOHtQfpQ",
       category: "Announcements",
       categoryId: "DIC_kwDOHtQfpc4CQZcs",
-
-      /**
-       * Using Twikoo
-       */
-      // provider: "Twikoo",
-      // envId: "https://twikoo.ccknbc.vercel.app",
-
-      /**
-       * Using Waline
-       */
-      // provider: "Waline",
-      // serverURL: "https://vuepress-theme-hope-comment.vercel.app",
     },
 
     mdEnhance: {
-      enableAll: true,
-      presentation: {
-        plugins: ["highlight", "math", "search", "notes", "zoom"],
-      },
+      figure: true,
+      tasklist: true,
     },
   },
 });
