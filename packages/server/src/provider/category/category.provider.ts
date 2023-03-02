b7b6b1b3c1a59cfae0111e219662a34c0f6f3219
Mr.Hope <mister-hope@outlook.com>
2023-03-03 01:53:13 +0800

    chore(server): remove unused import

--- packages/server/src/provider/category/category.provider.ts
+++ packages/server/src/provider/category/category.provider.ts
@@ -1,7 +1,6 @@
 import {
   Injectable,
   NotAcceptableException,
-  NotFoundException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
