b2900f0bc7d35dcf8b5821b49cea0605bfbb1445
Mr.Hope <mister-hope@outlook.com>
2023-03-03 12:42:00 +0800

    chore(website): refine search api

--- packages/website/api/search.ts
+++ packages/website/api/search.ts
@@ -1,14 +1,17 @@
-export async function searchArticles(str: string): Promise<any> {
+// TODO: Add return types
+export const searchArticles = async (query: string): Promise<any> => {
   try {
-    const url = `/api/public/search?value=${str}`;
-    const res = await fetch(url);
-    const { data } = await res.json();
+    const { data } = await fetch(`/api/public/search?value=${query}`).then(
+      (res) => res.json()
+    );
+
     return data.data;
   } catch (err) {
     console.log(err);
     throw err;
   }
-}
+};
+
 export async function searchWithApiRoute(str: string) {
   try {
     const url = `/api/search?value=${str}`;
