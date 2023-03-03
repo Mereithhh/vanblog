aacfdbe353a5de434e5c3aab79d2a380912d3d26
Mr.Hope <mister-hope@outlook.com>
2023-03-03 12:48:40 +0800

    chore(website): refine AlertCard

--- packages/website/components/AlertCard/index.tsx
+++ packages/website/components/AlertCard/index.tsx
@@ -1,21 +1,28 @@
-import dayjs from "dayjs";
+import Dayjs from "dayjs";
+
+// TODO: support expiration time
 export default function (props: {
   updatedAt: Date;
   createdAt: Date;
-  showExpirationReminder: boolean;
+  showExpirationReminder?: boolean;
+  expirationDays?: number;
 }) {
-  if (!props.showExpirationReminder) return null;
-  const today = dayjs();
-  const diff = today.diff(props.createdAt, "days");
-  if (diff > 30) {
-    return (
-      <div className="warning-card text-gray-600 dark:text-dark">
-        <div>
-          请注意，本文编写于 {diff} 天前，最后修改于{" "}
-          {today.diff(props.updatedAt, "days")} 天前，其中某些信息可能已经过时。
+  if (props.showExpirationReminder) {
+    const dayjs = Dayjs();
+    const diff = dayjs.diff(props.createdAt, "days");
+
+    if (diff > (props.expirationDays || 30)) {
+      return (
+        <div className="warning-card text-gray-600 dark:text-dark">
+          <div>
+            请注意，本文编写于 {diff} 天前，最后修改于{" "}
+            {dayjs.diff(props.updatedAt, "days")}{" "}
+            天前，其中某些信息可能已经过时。
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
+
   return null;
 }
