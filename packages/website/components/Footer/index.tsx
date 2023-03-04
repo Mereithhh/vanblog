7f9ba2bafa4e999a39da12b5b055e8ada1982613
Mr.Hope <mister-hope@outlook.com>
2023-03-04 14:13:48 +0800

    chore(website): refine Footer (#222)

--- packages/website/components/Footer/index.tsx
+++ packages/website/components/Footer/index.tsx
@@ -2,65 +2,74 @@ import ImageBox from "../ImageBox";
 import RunningTime from "../RunningTime";
 import Viewer from "../Viewer";
 
-export default function (props: {
-  ipcNumber: string;
-  ipcHref: string;
-  since: string;
-  version: string;
+export default function ({
+  ipcHref,
+  ipcNumber,
+  since,
+  version,
+  gaBeianLogoUrl,
+  gaBeianNumber,
+  gaBeianUrl,
+}: {
   // 公安备案
   gaBeianNumber: string;
   gaBeianUrl: string;
   gaBeianLogoUrl: string;
+  // ipc
+  ipcNumber: string;
+  ipcHref: string;
+  since: string;
+  version: string;
 }) {
   return (
     <>
       <footer className="text-center text-sm space-y-1 mt-8 md:mt-12 dark:text-dark">
-        {Boolean(props.ipcNumber) && props.ipcNumber != "" && (
+        {Boolean(ipcNumber) && (
           <p className="">
-            ICP 编号:{" "}
+            ICP 编号:&nbsp;
             <a
-              href={props.ipcHref}
+              href={ipcHref}
               target="_blank"
               className="hover:text-gray-900 hover:underline-offset-2 hover:underline dark:hover:text-dark-hover transition"
             >
-              {props.ipcNumber}
+              {ipcNumber}
             </a>
           </p>
         )}
-        {Boolean(props.gaBeianNumber) && props.gaBeianNumber != "" && (
+        {Boolean(gaBeianNumber) && (
           <p className="flex justify-center items-center">
-            公安备案:{" "}
-            {Boolean(props.gaBeianLogoUrl) && props.gaBeianLogoUrl != "" && (
+            公安备案:&nbsp;
+            {Boolean(gaBeianLogoUrl) && (
               <ImageBox
-                src={props.gaBeianLogoUrl}
+                src={gaBeianLogoUrl}
                 lazyLoad={true}
                 alt="公安备案 logo"
                 width={20}
               />
             )}
             <a
-              href={props.gaBeianUrl}
+              href={gaBeianUrl}
               target="_blank"
               className="hover:text-gray-900 hover:underline-offset-2 hover:underline dark:hover:text-dark-hover transition"
             >
-              {props.gaBeianNumber}
+              {gaBeianNumber}
             </a>
           </p>
         )}
-        <RunningTime since={props.since}></RunningTime>
+        <RunningTime since={since}></RunningTime>
         <p className="">
-          Powered By{" "}
+          Powered By&nbsp;
           <a
             href="https://vanblog.mereith.com"
             target={"_blank"}
-            className="hover:text-gray-900  dark:hover:text-dark-hover transition ua ua-link"
+            className="hover:text-gray-900 dark:hover:text-dark-hover transition ua ua-link"
           >
-            VanBlog <span>{props.version}</span>
+            VanBlog <span>{version}</span>
           </a>
         </p>
 
         <p className="select-none">
-          © {new Date(props.since).getFullYear()} - {new Date().getFullYear()}
+          © {new Date(since).getFullYear()} - {new Date().getFullYear()}
         </p>
         <p className="select-none">
           <Viewer></Viewer>
