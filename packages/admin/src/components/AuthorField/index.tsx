641d0ce4082440b451b0abbe432f6684d8120b72
Mr.Hope <mister-hope@outlook.com>
2023-03-08 23:01:38 +0800

    chore(admin): refine AuthorField

--- packages/admin/src/components/AuthorField/index.tsx
+++ packages/admin/src/components/AuthorField/index.tsx
@@ -1,25 +1,18 @@
-import { getAllCollaboratorsList } from '@/services/van-blog/api';
 import { ProFormSelect } from '@ant-design/pro-form';
-export default function () {
-  return (
-    <>
-      <ProFormSelect
-        width="md"
-        id="author"
-        name="author"
-        label="作者"
-        placeholder="不填默认为登录者本人"
-        request={async () => {
-          const msg = await getAllCollaboratorsList();
+import { getAllCollaboratorsList } from '@/services/van-blog/api';
 
-          return (
-            msg?.data?.map((item) => ({
-              label: item.nickname || item.name,
-              value: item.nickname || item.name,
-            })) || []
-          );
-        }}
-      />
-    </>
-  );
-}
+export default () => (
+  <ProFormSelect
+    width="md"
+    id="author"
+    name="author"
+    label="作者"
+    placeholder="不填默认为登录者本人"
+    request={async () =>
+      (await getAllCollaboratorsList())?.data?.map(({ name, nickname = name }) => ({
+        label: nickname,
+        value: nickname,
+      })) || []
+    }
+  />
+);
