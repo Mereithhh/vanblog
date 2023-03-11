f26ed55b280a4731ae568fb0d73d3c35da28ef39
Mr.Hope <mister-hope@outlook.com>
2023-03-11 13:49:12 +0800

    chore(admin): refine ArticleList (#235)

--- packages/admin/src/components/ArticleList/index.tsx
+++ packages/admin/src/components/ArticleList/index.tsx
@@ -1,27 +1,30 @@
 import { getRecentTimeDes } from '@/services/van-blog/tool';
 import './index.css';
-export default function (props: {
+
+export default ({
+  articles,
+  showViewerNum,
+  showRecentViewTime,
+}: {
+  // FIXME: Add Article type
   articles: any[];
   showViewerNum: boolean;
   showRecentViewTime: boolean;
-}) {
-  return (
-    <div>
-      {props.articles.map((article) => {
-        return (
-          <a
-            className="article-list-item  uaa"
-            key={article.id}
-            href={`/post/${article.id}`}
-            target={'_blank'}
-            rel="noreferrer"
-          >
-            <div className="">{article.title}</div>
-            {props.showViewerNum && <div>{`${article.viewer || 0}人次`}</div>}
-            {props.showRecentViewTime && <div>{getRecentTimeDes(article.lastVisitedTime)}</div>}
-          </a>
-        );
-      })}
-    </div>
-  );
-}
+}) => (
+  <div>
+    {articles.map(({ id, title, viewer = 0, lastVisitedTime }) => (
+      <a
+        // FIXME: uaa is not a good name
+        className="article-list-item uaa"
+        key={id}
+        href={`/post/${id}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div className="">{title}</div>
+        {showViewerNum && <div>{`${viewer || 0}人次`}</div>}
+        {showRecentViewTime && <div>{getRecentTimeDes(lastVisitedTime)}</div>}
+      </a>
+    ))}
+  </div>
+);
