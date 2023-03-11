72e4ef37c79874190da22dfc16764f249568b8cf
Mr.Hope <mister-hope@outlook.com>
2023-03-11 13:55:39 +0800

    chore(admin): refine CopyUploadBtn (#242)

--- packages/admin/src/components/CopyUploadBtn/index.tsx
+++ packages/admin/src/components/CopyUploadBtn/index.tsx
@@ -1,50 +1,54 @@
-import { getClipboardContents } from '@/services/van-blog/clipboard';
-import { Button, message } from 'antd';
+import { Button, message } from "antd";
 
-export default function (props: {
-  setLoading: Function;
-  onFinish: Function;
-  onError: Function;
+import { getClipboardContents } from "@/services/van-blog/clipboard";
+
+export interface CopyUploadBtnProps {
   url: string;
   accept: string;
   text: string;
-}) {
-  const handleClick = async (ev) => {
+  setLoading: (loading: boolean) => void;
+  onFinish: (data: unknown) => void;
+  onError: () => void;
+}
+
+export default function (props: CopyUploadBtnProps) {
+  const handleClick = async () => {
     props.setLoading(true);
+
     const fileObj = await getClipboardContents();
+
     if (!fileObj) {
       props.setLoading(false);
       props.onError();
       return;
     }
     const formData = new FormData();
-    formData.append('file', fileObj);
-    fetch('/api/admin/img/upload?withWaterMark=true', {
-      method: 'POST',
-      body: formData,
+
+    formData.append("file", fileObj);
+
+    return fetch("/api/admin/img/upload?withWaterMark=true", {
+      method: "POST",
       headers: {
-        token: (() => {
-          return window.localStorage.getItem('token') || 'null';
-        })(),
+        token: localStorage.getItem("token") || "null",
       },
+      body: formData,
     })
-      .then((res) => {
-        return res.json();
-      })
-      .then((res) => {
-        if (res.statusCode == 200) {
-          props?.onFinish(res?.data);
+      .then((res) => res.json())
+      .then(({ statusCode, data }) => {
+        if (statusCode === 200) {
+          props?.onFinish(data);
         } else {
-          message.error('上传失败！');
-          // console.log(res);
+          message.error("上传失败！");
         }
       })
-      .catch((err) => {
-        message.error('上传失败！');
-        // console.log(err);
+      .catch(() => {
+        message.error("上传失败！");
       })
-      .finally(() => [props.setLoading(false)]);
+      .finally(() => {
+        props.setLoading(false);
+      });
   };
+
   return (
     <div>
       <Button onClick={handleClick} type="primary">
