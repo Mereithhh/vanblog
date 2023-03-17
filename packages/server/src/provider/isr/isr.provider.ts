757abbafb6c794ac08ef1642c0d04f27b582510e
mereith <wanglu@mereith.com>
2023-03-17 11:50:16 +0800

    chore(server): 单独开发模式时，不触发 isr

--- packages/server/src/provider/isr/isr.provider.ts
+++ packages/server/src/provider/isr/isr.provider.ts
@@ -51,6 +51,9 @@ export class ISRProvider {
     this.logger.log('触发全量渲染完成！');
   }
   async activeAll(info?: string, delay?: number, activeConfig?: ActiveConfig) {
+    if (process.env["VANBLOG_DISABLE_WEBSITE"] === 'true') {
+      return ;
+    }
     if (this.timer) {
       clearTimeout(this.timer);
     }
