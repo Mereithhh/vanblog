2dd8b6ba52a396d3cc04a8d4a6c7cfef80bf9c6e
mereith <wanglu@mereith.com>
2023-04-12 19:53:34 +0800

    docs: update

---
+++ docs/.vuepress/component/TOC.vue
@@ -0,0 +1,12 @@
+<template>
+  <TOC>
+    <template #after>
+      <!-- <p style="margin-top: 16px;">广告位</p> -->
+     <div class="wwads-cn wwads-vertical" data-id="230" style="max-width:350px"></div>
+    
+    </template>
+  </TOC>
+</template>
+<script setup lang="ts">
+import TOC from "vuepress-theme-hope/modules/info/components/TOC.js";
+</script>
