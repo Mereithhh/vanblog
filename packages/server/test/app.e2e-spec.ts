52a6c1433c5818b8b5cf56f6c372a93d91d7df84
Mr.Hope <mister-hope@outlook.com>
2023-11-03 17:03:34 +0800

    chore: tweaks

--- packages/server/test/app.e2e-spec.ts
+++ packages/server/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
